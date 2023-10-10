90b6012ecf40e00dcffb8f45e0b1cfc334efdb27
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-10-10 23:45:43 +0530

    Spinner and Python Code Modified

--- app/src/main/java/com/example/pythonintegration/MainActivity.java
+++ app/src/main/java/com/example/pythonintegration/MainActivity.java
@@ -1,48 +1,53 @@
 package com.example.pythonintegration;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
-EditText Et1,Et2;
-Button btn;
-TextView tv;
-@Override
+    EditText Et1, Et2;
+    Button btn;
+    TextView tv;
+    Spinner operationSpinner;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Et1=findViewById(R.id.num1);
-        Et2=findViewById(R.id.num2);
-        btn=findViewById(R.id.button);
-        tv=findViewById(R.id.answer);
+        Et1 = findViewById(R.id.num1);
+        Et2 = findViewById(R.id.num2);
+        btn = findViewById(R.id.button);
+        tv = findViewById(R.id.answer);
+        operationSpinner = findViewById(R.id.operation_spinner);
 
-        if (! Python.isStarted()) {
+        if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
 
         Python py = Python.getInstance();
-        PyObject pyobj=py.getModule("script"); //Here we have to give the name of our python file
+        PyObject pyobj = py.getModule("script"); //Here we have to give the name of our Python file.
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.operations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        operationSpinner.setAdapter(adapter);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Function Name and Argument
-               PyObject obj=pyobj.callAttr("main", Et1.getText().toString(),
-                                                Et2.getText().toString()
-                                                );
-               tv.setText(obj.toString());
-
+                String operation = operationSpinner.getSelectedItem().toString();
+                PyObject obj = pyobj.callAttr("perform_operation", //Here we have to mention the Python Function.
+                        operation, Et1.getText().toString(), Et2.getText().toString()); //Maintain the order.
+                tv.setText(obj.toString());
             }
         });
     }
--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,7 +1,20 @@
 import numpy as np
-def main(number1,number2):
-    num1=int(number1)
-    num2=int(number2)
-    sum=num1+num2
-    return "Sum is"+ str(sum)
 
+def perform_operation(operation, number1, number2):
+    num1 = int(number1)
+    num2 = int(number2)
+    
+    if operation == "Addition":
+        result = num1 + num2
+    elif operation == "Multiplication":
+        result = num1 * num2
+    elif operation == "Subtraction":
+        result = num1 - num2
+    elif operation == "Division":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        result = num1 / num2
+    else:
+        return "Invalid operation"
+    
+    return f"Result of {operation} is {result}"
